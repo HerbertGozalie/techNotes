f17bc7c46f22f89f620d60aa5e5cf157185599e6
Herbert Gozalie <hbgozalie@gmail.com>
2024-12-10 00:39:28 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const root = require('./routes/root.js')
+const PORT = process.env.PORT || 3500
+
+//middleware
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use('/', root);
+
+//if the request is not match with the routes
+app.all('*', (req, res) => {
+  res.status(404)
+  if(req.accepts('html')){
+    res.sendFile(path.join(__dirname, 'views' ,'404.html'));
+  } else if(req.accepts('json')){
+    res.json({ message: '404 not found' });
+  } else{
+    res.type('txt').send('404 Not Found');
+  }
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on ${PORT}`);
+})
